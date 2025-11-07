f4f0a52caa615afafa93c7ef07efdd970cf94b30
junior djomo <juniordjomo07@gmail.com>
2025-11-07 16:12:16 +0100

    initialize project and create my first entity

---
+++ src/test/java/com/ecfjava/EcfjavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecfjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcfjavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
